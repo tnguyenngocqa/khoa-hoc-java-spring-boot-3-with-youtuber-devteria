c38c26ef17ac5bc6aed49a04283249e71b2183c6
tnguyenngocqa <51280278+tnguyenngocqa@users.noreply.github.com>
2024-07-14 09:01:01 +0700

    success

---
+++ src/main/java/com/devteria/identityservice/service/UserService.java
@@ -0,0 +1,53 @@
+package com.devteria.identityservice.service;
+
+import com.devteria.identityservice.dto.request.UserCreationRequest;
+import com.devteria.identityservice.dto.request.UserUpdateRequest;
+import com.devteria.identityservice.entity.User;
+import com.devteria.identityservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        // to create a new row in table in database
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        // Get user by id
+        User user = getUser(userId);
+        // Update info of that user
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
